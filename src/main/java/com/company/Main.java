bbee53ba1eafcdad271cdc85b87029203cce5782
TanyaFreebie <escurio@gmail.com>
2021-08-26 14:21:41 +0300

    all done

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,19 +2,15 @@ package com.company;
 
 
 
-import com.company.controlers.*;
-import com.company.dbhelper.DbConnection;
 import com.company.login.Login;
-import com.company.menu.Menu;
 
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Main {
    
+public static void main(String[] args){
+    //Test start
+
+Login.userLogin();
 
 //END OF TEST
 
