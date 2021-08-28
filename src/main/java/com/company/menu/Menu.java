d3e5480c78cf2765f34ed6b0feccd6ac7be6b625
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-08-29 00:19:40 +0300

    all case menu must be Strings(number can be a string) with X option to exit or return. Must avoid " do you wish to proceed(to many while running throught code)" Menu(X for logoff) - Submenu - X to return to Menu, after committing action - return to the start. There must be 3 or less actions to logout after committing action.

--- src/main/java/com/company/menu/Menu.java
+++ src/main/java/com/company/menu/Menu.java
@@ -18,27 +18,31 @@ public class Menu {
         System.out.println("4. Scores data");
         System.out.println("5. Events data");
         System.out.println("6. Observe data\n");
+        System.out.println("X. Exit");
 
         System.out.print("\nSelect an option");
-        int option = scanner.nextInt();
+        String option = scanner.next();
 
         switch (option) {
-            case 1:
+            case "1":
                 SubMenu.userSubMenu();
                 break;
-            case 2:
+            case "2":
                 SubMenu.employeeSubMenu();
                 break;
-            case 3:
+            case "3":
 //                SubMenu.studentSubMenu();
                 break;
-            case 4:
+            case "4":
                 SubMenu.scoreSubMenu();
                 break;
-            case 5:
+            case "5":
                 SubMenu.eventSubMenu();
                 break;
-            case 6:
+            case "6":
+                SubMenu.observeData();
+                break;
+            case "X":
                 SubMenu.observeData();
                 break;
 
